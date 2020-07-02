30e6bbd21f25f840fd11ce35119eef8c5e915473
krishna-dalam <krishnachaithanyadalam@gmail.com>
2020-07-02 20:05:33 +0530

    firebase code

--- src/firebase/connect.js
+++ src/firebase/connect.js
@@ -1,38 +1,38 @@
-// import { useState, useEffect } from "react";
-// import useFirebase from "./useFirebase";
+import { useState, useEffect } from "react";
+import useFirebase from "./useFirebase";
 
-// const firebase = useFirebase();
+const firebase = useFirebase();
 
-// const getData = (collection) => {
-//   const [data, setData] = useState([]);
+const getData = (collection) => {
+  const [data, setData] = useState([]);
 
-//   const get = async () => {
-//     try {
-//       await firebase
-//         .database()
-//         .ref(collection)
-//         .on("value", (documents) => {
-//           let fetchedUsers = [];
-//           documents.forEach((child) => {
-//             fetchedUsers.push({ ...child.val() });
-//           });
-//           setData(fetchedUsers);
-//         });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   useEffect(() => {
-//     get();
-//   }, []);
-//   return data;
-// };
+  const get = async () => {
+    try {
+      await firebase
+        .database()
+        .ref(collection)
+        .on("value", (documents) => {
+          let fetchedUsers = [];
+          documents.forEach((child) => {
+            fetchedUsers.push({ ...child.val() });
+          });
+          setData(fetchedUsers);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    get();
+  }, []);
+  return data;
+};
 
-// const putData = (collection, data) => {
-//   firebase
-//     .database()
-//     .ref(collection + "/" + data.id)
-//     .set(data);
-// };
+const putData = (collection, data) => {
+  firebase
+    .database()
+    .ref(collection + "/" + data.id)
+    .set(data);
+};
 
-// export { getData, putData };
+export { getData, putData };
